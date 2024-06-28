0f68663ce0dcab99b61ed92057f16952c5c9caa7
the2minengineer <ifeanyiemmanueljoseph@gmail.com>
2024-06-28 23:44:36 +0100

    initialize socket.io

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
 const router = require("./routes/route");
+
 
 app.use(express.json());
 app.use(cors());
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/database");
+const { Server } = require("socket.io");
+
+const io = new Server(server);
 
 const { PORT } = process.env;
 
 connectDB().then(() => {
 	app.listen(PORT, () => {
 		console.info(`Server is listening on PORT ${PORT}`);
+	});
+
+	io.on("connection", (socket) => {
+		console.log("a user connected");
 	});
 });
